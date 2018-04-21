16e9f84783f02089df3657121b76ea415bd75faf
Josh <jhint2424@gmail.com>
2018-04-21 02:22:16 -0700

    adding display function

--- displayProduct.js
+++ displayProduct.js
@@ -1,19 +1,28 @@
 var connection = require("./connection");
 function displayProducts()  {
     console.log("All Products For Sale: \n");
-    connection.query("SELECT * FROM products", function(err, res)   {
+    var query = "SELECT * FROM products";
+    connection.query(query, function(err, res)   {
         if (err)    {
             console.log(err);
         }
         else    {
 
-            console.log(res);
-            // for (var i = 0; i = res.length; i++)    {
-            //     console.log(res[i]);
-            //     }            
+            // console.log(res);
+            // // for (var i = 0; i = res.length; i++)    {
+            //     console.log(res[i].item_id);
+            //     }       
+            var itemArray = [];
+            for (var i = 0; i < res.length; i++) {
+              itemArray.push(res[i].product_name);
+              itemArray.push(res[i].item_id);
+              itemArray.push(res[i].price);
+
+              console.log(itemArray);
+            }     
             connection.end();
         }
-    } )
+    })
 }
 
 
