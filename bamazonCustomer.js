da83a91f8afa87b32ac5255fb69716416375b4cb
Josh <jhint2424@gmail.com>
2018-04-21 15:30:41 -0700

    display done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
 var connection = require("./connection");
 var displayProducts = require("./displayProduct");
 
-displayProducts;
+displayProducts();
--- displayProduct.js
+++ displayProduct.js
@@ -1,6 +1,12 @@
 var connection = require("./connection");
+var Table = require('cli-table');
+var table = new Table({
+    head: ['ID', 'Product', 'Department', 'Price', 'Quantity']
+});
+
+// console.log(table.toString());
 function displayProducts()  {
-    console.log("All Products For Sale: \n");
+    console.log("\n All Products For Sale: \n");
     var query = "SELECT * FROM products";
     connection.query(query, function(err, res)   {
         if (err)    {
@@ -8,22 +14,30 @@ function displayProducts()  {
         }
         else    {
 
-            // console.log(res);
+            
+            //console.log(res);
             // // for (var i = 0; i = res.length; i++)    {
             //     console.log(res[i].item_id);
             //     }       
             var itemArray = [];
             for (var i = 0; i < res.length; i++) {
-              itemArray.push(res[i].product_name);
-              itemArray.push(res[i].item_id);
-              itemArray.push(res[i].price);
+            table.push([res[i].item_id, res[i].product_name,res[i].department_name, res[i].price, res[i].stock_quantity]);   
+            //   itemArray.push(res[i].item_id);                
+            //   itemArray.push(res[i].product_name);
+            //   itemArray.push(res[i].department_name);
+            //   itemArray.push(res[i].price);
+            //   itemArray.push(res[i].stock_quantity);
+            //  console.log(itemArray);
 
-              console.log(itemArray);
+
+
             }     
+            console.log(table.toString());
+            //console.log(itemArray);
             connection.end();
         }
     })
 }
 
 
-module.exports = displayProducts();
+module.exports = displayProducts;
