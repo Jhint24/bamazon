bdf631128c48e537cfccf67fc7ae12fb0d4679a3
Josh <jhint2424@gmail.com>
2018-04-23 13:52:57 -0700

    adding validate functionality

--- displayProduct.js
+++ displayProduct.js
@@ -1,11 +1,12 @@
 var connection = require("./connection");
 var inquirer = require('inquirer');
 var Table = require('cli-table');
+var mysql = require('mysql');
 var table = new Table({
     head: ['ID', 'Product', 'Department', 'Price', 'Quantity']
 });
 
-// console.log(table.toString());
+
 function displayProducts()  {
     console.log("\n All Products For Sale: \n");
     var query = "SELECT * FROM products";
@@ -17,60 +18,87 @@ function displayProducts()  {
             var itemArray = [];
             for (var i = 0; i < res.length; i++) {
             table.push([res[i].item_id, res[i].product_name,res[i].department_name, res[i].price, res[i].stock_quantity]);
-    }     
+    }   
+
             console.log(table.toString());
 
-                inquirer.prompt([
-                    {
-                    type: "input",
-                    name: "productId",
-                    message: "Please input the Product ID you would like to purchase..."
-                    },
-                    {
-                    type: "input",
-                    name: "productQuantity",
-                    message: "How many would you like to purchase?"
-                    }
-                ])
-                    .then(function(answer)    {
-                        var chosenProduct;
-                        for (var i = 0; i < res[i].length; i++)  {
-                            if (res[i].item_id === answer.productId)    {
-                                chosenProduct = res[i];
-                            }
-                        
-                        var newQuantity = res[i].stock_quantity - chosenProduct.stock_quantity;
-                        if (chosenProduct.stock_quantity < res[i].stock_quantity)   {
-                            connection.query(
-                                "UPDATE products SET ? WHERE ?",
-                                [
-                                    {
-                                        stock_quantity: newQuantity
-                                    },
-                                    {
-                                        item_id: productId
-                                    }
-                                ],
-                                function(error) {
-                                    if (error) throw err;
-                                    console.log("Purchase Completed Successfully");
-                                }
-                            
-                            );
-                        }
-                            else {
-                                console.log("There is not enough of this product...")
-                            }
-                        }
-                        //if (user.productQuantity <= res[i].stock_quantity)  {
-                    // console.log(answer.productQuantity + " " + answer.productId + " purchased")
-                        //}
-                });
-
-            connection.end();
         }
+        inquirer.prompt([
+            {
+            type: "input",
+            name: "productId",
+            message: "Please input the Product ID you would like to purchase..."
+            },
+            {
+            type: "input",
+            name: "productQuantity",
+            message: "How many would you like to purchase?"
+            }
+        ])
+        .then(function(answer)  {
+            if (answer.productId >= res.length) {
+                console.log("Please input a correct Product ID");
+                displayProducts();
+            }
+            else {
+                //validateQuantity();
+                console.log("valid id number")
+                connection.end();  
+            }
+        })
     })
+    }    
+
+function validateQuantity(val) {
+    var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?;";
+    connection.query(query, { item_id: val.id }, function(err, res) {
+        if (err) throw err;
+    
+        if (val.productQuantity > res[0].stock_quantity) {
+          console.log("Please input a number at or below the current quantity");
+          buyProduct();
+        }
+    
+        updateQuantity(val, res);
+      });
 }
 
 
+
 module.exports = displayProducts;
+
+
+//     .then(function(answer)    {
+//         var chosenProduct;
+//         for (var i = 0; i < res[i].length; i++)  {
+//             if (res[i].item_id === answer.productId)    {
+//                 chosenProduct = res[i];
+//             }
+        
+//         var newQuantity = res[i].stock_quantity - chosenProduct.stock_quantity;
+//         if (chosenProduct.stock_quantity < res[i].stock_quantity)   {
+//             connection.query(
+//                 "UPDATE products SET ? WHERE ?",
+//                 [
+//                     {
+//                         stock_quantity: newQuantity
+//                     },
+//                     {
+//                         item_id: productId
+//                     }
+//                 ],
+//                 function(error) {
+//                     if (error) throw err;
+//                     console.log("Purchase Completed Successfully");
+//                 }
+            
+//             );
+//         }
+//             else {
+//                 console.log("There is not enough of this product...")
+//             }
+//         }
+//         //if (user.productQuantity <= res[i].stock_quantity)  {
+//     // console.log(answer.productQuantity + " " + answer.productId + " purchased")
+//         //}
+// });
